bdb3582b6bdefaa536509f0306aa089ce7a504eb
mazsibazsi <32135909+mazsibazsi@users.noreply.github.com>
2024-02-08 15:20:19 +0100

    chore(note): amend checkstyle errors in NoteRepository

--- src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/NoteRepository.java
+++ src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/NoteRepository.java
@@ -2,16 +2,23 @@ package io.muehlbachler.fhburgenland.swm.examination.repository;
 
 import java.util.List;
 
+import io.muehlbachler.fhburgenland.swm.examination.model.Person;
 import org.springframework.data.repository.CrudRepository;
 
 import io.muehlbachler.fhburgenland.swm.examination.model.Note;
 
+/**
+ * Representing the repository for persisting a {@link Person},
+ * identified by a {@link String} ID.
+ */
 public interface NoteRepository extends CrudRepository<Note, String> {
 
     /**
      * Finds {@link Note}s, that contain the searched content from the parameter.
+     *
      * @param content {@link String} The content to be looked for in the {@link Note}s
-     * @return A {@link List} of {@link Note}s, that contain the content given in the param. Can be empty.
+     * @return A {@link List} of {@link Note}s, that contain the content given in the param.
+     *              Can be empty.
      */
     List<Note> findByContentContaining(String content);
 }
