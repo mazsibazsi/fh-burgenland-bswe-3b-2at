0f31b2d744656140a62b8e17f7805112195c3ab4
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2024-02-15 14:05:13 +0100

    chore(ci): check pipeline

--- src/test/java/io/muehlbachler/fhburgenland/swm/examination/controller/PersonControllerTest.java
+++ src/test/java/io/muehlbachler/fhburgenland/swm/examination/controller/PersonControllerTest.java
@@ -41,6 +41,18 @@ public class PersonControllerTest {
     }
 
     /**
+     * Tests the getbyID method using
+     * a known person ID to ensure it retrieves the person successfully.
+     */
+    @Test
+    void testGetByIdNotFound() {
+        ResponseEntity<Person> person =
+		personController.get("81150016-8501-4b97-9168-01113e21d8a2");
+
+        assertEquals(HttpStatus.NOT_FOUND, person.getStatusCode(), "person should not be found");
+    }
+
+    /**
      * Tests, if all {@link Person}s are received.
      */
     @Test
